6e78669eb7a9707cfa27604735d521172a4a3054
jcg8a <jc.gaitan2729@gmail.com>
2022-09-28 15:15:31 -0300

    New implementation of function: validate_most

--- aux_validate_functions.R
+++ aux_validate_functions.R
@@ -77,31 +77,22 @@
 # Aux validate rank most
 #-------------------------------------------------------------------------------
 
-# Input: .df should be a data frame of multiple columns (as many as the target question has)
+# Input: Two df: one for check all that apply columns. Other for ranking columns
 # Output: an standardized named vector.
 
-.validate_most <- function(.survey, .pre, .post){
-  aux_pre <- .filter_columns(.df = .survey, .loop_question = .pre)
-  aux_pre <- select(aux_pre, -stringr::str_which(colnames(aux_pre), "_text"))
+.validate_most <- function(.df_pre, .df_post){
+  aux_most <- tibble(pre_count = apply(.df_pre, 1, function(x) sum(x != "0")))
+  aux_most <- mutate(aux_most, pre_complete = if_else(pre_count > 1, 1L, 0L, missing = NA_integer_),
+                     post_count = apply(.df_post, 1, function(x) sum(x != "0")),
+                     post_complete = if_else(post_count > 0, 1L, NA_integer_, missing = NA_integer_))
 
-  aux_post <- .filter_columns(.df = .survey, .loop_question = .post)
-  aux_post <- select(aux_post, -stringr::str_which(colnames(aux_post), "_text"))
+  aux_most[ ,"result_col"] <- aux_most[, "pre_complete"] - aux_most[, "post_complete"]
 
-  aux_pre <- mutate(aux_pre, result_col_pre_aux = apply(aux_pre, 1, function(x) sum(x != "0")))
-  aux_pre <- mutate(aux_pre, result_col_pre = if_else(aux_pre[, "result_col_pre_aux"] > 1, 1L, 0L, missing = 0L))
-  aux <- aux_pre[, "result_col_pre"]
+  out <- c(sum(aux_most[, "pre_complete"], na.rm = TRUE),
+           sum(aux_most[, "post_complete"], na.rm = TRUE),
+           sum(aux_most[, "result_col"] != 0, na.rm = TRUE),
+           sum(aux_most[, "result_col"] == 1, na.rm = TRUE),
+           sum(aux_most[, "result_col"] == -1, na.rm = TRUE))
 
-  # I dont worry about the column index because rank is always a select one question, isn't it?
-  out <- mutate(aux_post, result_col_post = if_else(is.na(aux_post[, 1]), 0L, 1L, missing = 0L))
-  out <- out[, "result_col_post"]
-
-  out <- bind_cols(aux, out)
-
-  out <- mutate(out, result = out[ , "result_col_pre"] - out[ , "result_col_post"])
-
-  result <- sum(out[, 3] != 0)
-  respondent_pre <- sum(out[, 1])
-  respondent_post <- sum(out[, 2])
-  result_forward <- sum(out[, 3] == 1)
-  result_backward <- sum(out[, 3] == -1)
+  names(out) <- c("respondent_pre", "respondent_post", "result", "result_forward", "result_backward")
 }
