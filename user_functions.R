df0ccc6c35ac1d4967155688814ae7fd9fc630f2
jcg8a <jc.gaitan2729@gmail.com>
2022-10-06 21:04:33 -0300

    Modify user function binarize: input formatting returns a vector of questions and a list of options.

--- user_functions.R
+++ user_functions.R
@@ -1,15 +1,17 @@
 
 binarize <- function(..., survey = tb_survey){
   # 1. Common validators
-  # Formatting the input
+  # 2. Input formatting
+  q_input <- ...names()
   input <- list(...)
-  # 2. Column selection
+  # 3. Column selection
   tables <- list()
-  for (i in 1:length(input)) {
-    tables[[i]] <- .filter_columns(survey, input[[i]])
+  for (i in 1:length(q_input)) {
+    tables[[i]] <- .filter_columns(survey, q_input[i])
   }
-  # 3. Additional validations
-  # 4. Main function
+  # 4. Additional validations
+  # 5. Main function
+  out <- .join_binaries(tables, input)
   # 5. Output
-  return(tables)
+  return(out)
 }
