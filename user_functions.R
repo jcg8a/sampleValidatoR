0dcb3e4a3c8cd6fb07f3ac63295a73119dcdcdc9
jcg8a <jc.gaitan2729@gmail.com>
2022-10-10 20:08:06 -0300

    Create new user function: v_other. It validates questions with other, please specify options

--- user_functions.R
+++ user_functions.R
@@ -15,3 +15,37 @@ binarize <- function(..., survey = tb_survey){
   # 5. Output
   return(out)
 }
+
+
+
+
+
+
+
+
+
+
+
+#-------------------------------------------------------------------------------
+# User function validate other
+#-------------------------------------------------------------------------------
+
+v_other <- function(..., survey = tb_survey, text_other = "Other, please specify:", autofiller = "-99"){
+  # 1. Common validators
+  # 2. Input formatting
+  input <- list(...)
+  # 3. Column selection
+  tables <- list()
+  for(i in 1:length(input)){
+    tables[[i]] <- .filter_columns_other(survey, input[[i]], .other = text_other)
+  }
+  # 4. Additional validations
+  # 5. Main function
+  for (i in 1:length(tables)) {
+    for (j in 1:length(tables[[i]])) {
+      tables[[i]][[j]] <- .validate_other(.df = tables[[i]][[j]], .text_other = text_other, .autofiller = autofiller)
+    }
+  }
+  # 6. Output
+  return(tables)
+}
